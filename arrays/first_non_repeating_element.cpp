4cf1238625512db3fdf100ecfbb813396dbc10ec
Gugulothu Naga Saidulu <your-email@example.com>
2026-05-15 08:02:11 +0530

    Added first non repeating element program

---
+++ arrays/first_non_repeating_element.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={1,2,3,4,5};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n;i++)
+    {
+        int count=0;
+        for(int j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+            count++;
+            }
+        }
+        if(count==1)
+            {
+                cout<<arr[i]<<" ";
+                return 0;
+            }
+    }
+    return 0;
+}
