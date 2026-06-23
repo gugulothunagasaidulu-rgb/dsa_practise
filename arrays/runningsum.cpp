b0cda24298966b54c03e62c7691455e1c80560f2
Gugulothu Naga Saidulu <your-email@example.com>
2026-06-23 22:21:10 +0530

    added logic of runningsum

---
+++ arrays/runningsum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int nums[]={1,2,3,4};
+    int n=sizeof(nums)/sizeof(nums[0]);
+    for(int i=0;i<n;i++ )
+    {
+        nums[i]=nums[i]+nums[i-1];
+        cout<<nums[i]<<" ";
+    }
+    
+}
